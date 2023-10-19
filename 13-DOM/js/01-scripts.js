ba52a4c783422740cb2e8a63efc2fc8808e0d7b9
sesp13 <sespinosarte99@gmail.com>
2023-10-18 22:44:48 -0500

    primeros pasos con el DOM

---
+++ 13-DOM/js/01-scripts.js
@@ -0,0 +1,17 @@
+let elemento;
+
+elemento = document.all;
+elemento = document.head;
+elemento = document.body;
+elemento = document.forms;
+elemento = document.forms[0].id;
+elemento = document.forms[0].method;
+elemento = document.forms[0].classList;
+elemento = document.forms[0].action;
+
+elemento = document.links;
+elemento = document.links[4];
+elemento = document.links[4].classList;
+elemento = document.links[4].className;
+
+console.log(elemento);
---
+++ 13-DOM/js/02-scripts.js
@@ -0,0 +1,13 @@
+// Seleccionar elementos por su clase
+
+const header = document.getElementsByClassName('header');
+console.log(header);
+
+const hero = document.getElementsByClassName('hero');
+console.log(hero);
+
+const contenedores = document.getElementsByClassName('contenedor');
+console.log(contenedores);
+
+const noExiste = document.getElementsByClassName('no-existe');
+console.log(noExiste);
