d3f06ebbf04f1970862fb77fb82688881e44410e
sesp13 <sespinosarte99@gmail.com>
2024-02-26 23:14:34 -0500

    empezar paginador para proyecto de pixabay

--- 38-PROYECTO-PixabayImagenes/js/app.js
+++ 38-PROYECTO-PixabayImagenes/js/app.js
@@ -2,6 +2,8 @@ import { CLAVE } from '../clave.js';
 
 const formulario = document.querySelector('#formulario');
 const resultado = document.querySelector('#resultado');
+const registrosPorPagina = 40;
+let totalPaginas = 0;
 
 window.onload = () => {
   formulario.addEventListener('submit', validarFormulario);
@@ -17,7 +19,7 @@ function validarFormulario(e) {
     return;
   }
 
-  buscarImagenes();
+  buscarImagenes(terminoBusqueda);
 }
 
 function mostrarAlerta(mensaje) {
@@ -53,20 +55,67 @@ function mostrarAlerta(mensaje) {
 
 function buscarImagenes(termino) {
   const key = CLAVE.PIXABAY_KEY;
-  const url = `https://pixabay.com/api/?key=${key}&q=${termino}`;
+  const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=${100}`;
 
   fetch(url)
-  .then(res => res.json())
-  .then(resultado => mostrarImagenes(resultado.hits))
+    .then((res) => res.json())
+    .then((resultado) => {
+      totalPaginas = calcularPaginas(resultado.totalHits);
+      console.log(totalPaginas);
+      mostrarImagenes(resultado.hits);
+    });
 }
 
 function mostrarImagenes(imagenes) {
   limpiarHtml();
-  console.log(imagenes);
+
+  imagenes.forEach((imagen) => {
+    const { previewURL, likes, views, largeImageURL } = imagen;
+
+    resultado.innerHTML += `
+    <div class="w-1/2 md:w-1/3 lg:w-1/4 p-3 mb-4">
+      <div class="bg-white">
+        <img class="w-full" src="${previewURL}">
+        <div class="p-4">
+          <p class="font-bold">
+            ${likes} <span class="font-light"> Me Gusta </span>
+          </p>
+          <p class="font-bold">
+            ${views} <span class="font-light"> Veces vista </span>
+          </p>
+          <a 
+            class="
+              block
+              w-full 
+              bg-blue-800 
+              hover:bg-blue-500 
+              text-white 
+              uppercase
+              font-bold
+              text-center
+              rounded
+              mt-5
+              p-1
+            "
+            href="${largeImageURL}" 
+            target="_blank" 
+            rel="noopener noreferer"
+          >
+            Ver Imagen
+          </a>
+        </div>
+      </div>
+    </div>
+    `;
+  });
 }
 
 function limpiarHtml() {
   while (resultado.firstChild) {
     resultado.removeChild(resultado.firstChild);
   }
+}
+
+function calcularPaginas(total) {
+  return Math.ceil(total / registrosPorPagina);
 }
