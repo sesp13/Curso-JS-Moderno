d403f4e5aebec9e8ff34ed1dc9794438b8051151
Santiago Espinosa <santiago.espinosa@globant.com>
2024-03-20 15:52:15 -0500

    inicio proyecto crud async await

---
+++ 44-PROYECTO-CRM-CRUD-REST/js/funciones.js
@@ -0,0 +1,32 @@
+export function mostrarAlerta(mensaje) {
+  const existeAlerta = document.querySelector('.bg-red-100');
+
+  if (!existeAlerta) {
+    const alerta = document.createElement('p');
+    alerta.classList.add(
+      'bg-red-100',
+      'border-red-400',
+      'text-red-700',
+      'px-4',
+      'py-3',
+      'rounded',
+      'max-w-lg',
+      'mx-auto',
+      'mt-6',
+      'text-center'
+    );
+
+    alerta.innerHTML = `
+      <strong class="font-bold">Error!</strong>
+      <br />
+      <strong class="block sm:inline">${mensaje}</strong>
+    `;
+
+    const formulario = document.querySelector('#formulario');
+    formulario.appendChild(alerta);
+
+    setTimeout(() => {
+      alerta.remove();
+    }, 3000);
+  }
+}
---
+++ 44-PROYECTO-CRM-CRUD-REST/js/nuevocliente.js
@@ -0,0 +1,26 @@
+import { mostrarAlerta } from "./funciones.js";
+
+(() => {
+  const formulario = document.querySelector('#formulario');
+
+  formulario.addEventListener('submit', validarCliente);
+
+  function validarCliente(e) {
+    e.preventDefault();
+
+    const nombre = document.querySelector('#nombre').value;
+    const email = document.querySelector('#email').value;
+    const telefono = document.querySelector('#telefono').value;
+    const empresa = document.querySelector('#empresa').value;
+
+    const cliente = { nombre, email, telefono, empresa };
+
+    if (validar(cliente)) {
+      mostrarAlerta('Todos los campos son obligatorios');
+    }
+  }
+
+  function validar(obj) {
+    return !Object.values(obj).every((input) => input !== '');
+  }
+})();
