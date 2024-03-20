4f8522558a2fc7bd74448fd1742d26a97ff5b7b3
sesp13 <sespinosarte99@gmail.com>
2024-03-19 21:50:28 -0500

    avances con la calculadora de propinas

--- 40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
+++ 40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
@@ -130,6 +130,137 @@ function mostrarPlatillos(platillos) {
     } else {
       cliente.pedido = pedido.filter((articulo) => articulo.id !== producto.id);
     }
-    console.log(cliente.pedido);
+
+    // Limpiar el html previo
+    limpiarHtml();
+
+    actualizarResumen();
   }
+}
+
+function actualizarResumen() {
+  const contenido = document.querySelector('#resumen .contenido');
+
+  const resumen = document.createElement('div');
+  resumen.classList.add('col-md-6', 'card', 'py-5', 'px-3', 'shadow');
+
+  // Información de la esa
+  const mesa = document.createElement('p');
+  mesa.textContent = `Mesa: `;
+  mesa.classList.add('fw-bold');
+
+  const mesaSpan = document.createElement('span');
+  mesaSpan.textContent = cliente.mesa;
+  mesa.classList.add('fw-normal');
+
+  // Información de la hora
+  const hora = document.createElement('p');
+  hora.textContent = `Hora: `;
+  hora.classList.add('fw-bold');
+
+  const horaSpan = document.createElement('span');
+  horaSpan.textContent = cliente.hora;
+  hora.classList.add('fw-normal');
+
+  // Agregar a los elementos padre
+  mesa.appendChild(mesaSpan);
+  hora.appendChild(horaSpan);
+
+  // Titulo de la seccion
+  const heading = document.createElement('h3');
+  heading.textContent = 'Platillos consumidos';
+  heading.classList.add('my-4', 'text-center');
+
+  // Iterar sobre los pedidos
+  const grupo = document.createElement('ul');
+  grupo.classList.add('list-group');
+  const { pedido } = cliente;
+  pedido.forEach((articulo) => {
+    const { nombre, cantidad, precio, id } = articulo;
+
+    const lista = document.createElement('li');
+    lista.classList.add('list-group-item');
+
+    // Titulo del articulo
+    const nombreEl = document.createElement('h4');
+    nombreEl.classList.add('my-4');
+    nombreEl.textContent = nombre;
+
+    // Cantidad del articulo
+    const cantidadEl = document.createElement('p');
+    cantidadEl.classList.add('fw-bold');
+    cantidadEl.textContent = `Cantidad: `;
+
+    const cantidadValor = document.createElement('span');
+    cantidadValor.classList.add('fw-normal');
+    cantidadValor.textContent = cantidad;
+
+    // Precio del articulo
+    const precioEl = document.createElement('p');
+    precioEl.classList.add('fw-bold');
+    precioEl.textContent = `Precio: `;
+
+    const precioValor = document.createElement('span');
+    precioValor.classList.add('fw-normal');
+    precioValor.textContent = `$${precio}`;
+
+    // Subotal del articulo
+    const subtotalEl = document.createElement('p');
+    subtotalEl.classList.add('fw-bold');
+    subtotalEl.textContent = `Subtotal: `;
+
+    const subtotalValor = document.createElement('span');
+    subtotalValor.classList.add('fw-normal');
+    subtotalValor.textContent = calularSubtotal(precio, cantidad);
+
+    // Boton para eliminar
+    const btnEliminar = document.createElement('button');
+    btnEliminar.classList.add('btn', 'btn-danger');
+    btnEliminar.textContent = 'Eliminar del pedido';
+    btnEliminar.onclick = function () {
+      eliminarProducto(id);
+    };
+
+    // Agregar valores a sus contenedores
+    cantidadEl.appendChild(cantidadValor);
+    precioEl.appendChild(precioValor);
+    subtotalEl.appendChild(subtotalValor);
+
+    // Agregar elementos al li
+    lista.appendChild(nombreEl);
+    lista.appendChild(cantidadEl);
+    lista.appendChild(precioEl);
+    lista.appendChild(subtotalEl);
+    lista.appendChild(btnEliminar);
+
+    // Agregar lista al grupo principal
+    grupo.appendChild(lista);
+  });
+
+  // Agregar al contenedor
+  resumen.appendChild(mesa);
+  resumen.appendChild(hora);
+  resumen.appendChild(heading);
+  resumen.appendChild(grupo);
+
+  contenido.appendChild(resumen);
+}
+
+function limpiarHtml() {
+  const contenido = document.querySelector('#resumen .contenido');
+  while (contenido.firstChild) {
+    contenido.removeChild(contenido.firstChild);
+  }
+}
+
+function calularSubtotal(precio, cantidad) {
+  return `$ ${precio * cantidad}`;
+}
+
+function eliminarProducto(id) {
+  const { pedido } = cliente;
+  const resultado = pedido.filter((articulo) => articulo.id !== id);
+  cliente.pedido = [...resultado];
+  limpiarHtml();
+  actualizarResumen();
 }
